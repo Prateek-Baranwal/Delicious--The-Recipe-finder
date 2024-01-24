4af890a369c65f87844727dbebabc1f4267b136f
Prateek-Baranwal <154677182+Prateek-Baranwal@users.noreply.github.com>
2024-01-25 02:18:15 +0530

    Add files via upload

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://media.istockphoto.com/id/1490291782/vector/recipe-or-menu-page-letterhead-design.jpg?s=2048x2048&w=is&k=20&c=y67AQDuwB9v9YP4GvS0Nhs8TfC0LTqDRmpISX6POXgE=';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
